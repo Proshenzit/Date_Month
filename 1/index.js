275b81524673990f55d2b7c962aae56c4ca179b5
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-20 09:42:04 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,28 @@
+var date = new Date();
+console.log(date);
+
+var year =date.getFullYear();
+
+console.log(year);
+
+var carrentDate =date.getDate();
+
+console.log(carrentDate);
+
+var carrentDay = date.getDay();
+
+console.log(carrentDay);
+
+
+
+var currentHours = date.getHours();
+
+console.log(currentHours);
+
+var currentMinutes = date.getMinutes();
+
+console.log(currentMinutes);
+
+
+
+
